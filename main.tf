93c1ba98f6653a879c3e33819fc863c34ccb44b4
aristidejou <106462048+aristidejou@users.noreply.github.com>
2022-08-27 20:54:30 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+profile ="aristide"
+region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+bucket = ""
+acl = "private"
+}
+tags = {
+  Name = "My bucket"
+  Environment = "Dev"
+ }
+}
